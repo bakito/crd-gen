8a7928423109a2728901d163420821acc4776f5d
bakito <github@bakito.ch>
2025-04-23 22:32:37 +0200

    initial commit

---
+++ cmd/extract-crd-api/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+type arrayFlags []string
+
+// String is an implementation of the flag.Value interface.
+func (i *arrayFlags) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+// Set is an implementation of the flag.Value interface.
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+var (
+	excludeFlags arrayFlags
+	module       string
+	path         string
+	target       string
+	clearTarget  = false
+	useGit       = false
+)
+
+func main() {
+	flag.Var(&excludeFlags, "exclude", "Regex pattern for file excludes")
+	flag.StringVar(&module, "module", "", "The go module to get the api files from")
+	flag.StringVar(&path, "path", "", "The path within the module to the api files")
+	flag.StringVar(&target, "target", "", "The target directory to copyFile the files to")
+	flag.BoolVar(&clearTarget, "clear", false, "Clear target dir")
+	flag.BoolVar(&useGit, "use-git", false, "Use git instead of go mod (of module is not proper versioned)")
+	flag.Parse()
+
+	var excludes []*regexp.Regexp
+	for _, excludeFlag := range excludeFlags {
+		excludes = append(excludes, regexp.MustCompile(excludeFlag))
+	}
+
+	tmp, err := os.MkdirTemp("", "crd-gen")
+	if err != nil {
+		slog.Error("Failed to create a temp dir", "error", err)
+		return
+	}
+	defer func() { _ = os.RemoveAll(tmp) }()
+	moduleRoot := tmp
+
+	if useGit {
+		slog.With("module", module, "tmp", tmp).Info("Cloning module")
+		info := strings.Split(module, "@")
+		r, err := git.PlainClone(tmp, false, &git.CloneOptions{
+			URL:      "https://" + info[0],
+			Progress: os.Stdout,
+		})
+		if err != nil {
+			slog.Error("Failed to clone module", "error", err)
+			return
+		}
+		w, err := r.Worktree()
+		if err != nil {
+			slog.Error("Failed to clone module", "error", err)
+			return
+		}
+		if len(info) > 0 {
+			err = w.Checkout(&git.CheckoutOptions{
+				Branch: plumbing.NewTagReferenceName(info[1]),
+			})
+			if err != nil {
+				slog.With("tag", info[1]).Error("Failed to checkout tag", "error", err)
+				return
+			}
+		}
+	} else {
+		cmd := exec.Command("go", "mod", "download", module)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		cmd.Env = append(os.Environ(), "GOMODCACHE="+tmp)
+
+		slog.With("module", module, "tmp", tmp).Info("Downloading")
+		err = cmd.Run()
+		if err != nil {
+			slog.Error("Failed to download module", "error", err)
+			return
+		}
+		cmd = exec.Command("chmod", "+w", "-R", tmp)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			slog.Error("Failed to download module", "error", err)
+			return
+		}
+		moduleRoot = filepath.Join(tmp, module)
+	}
+	slog.Info("Module downloaded successfully!")
+
+	apiPath := filepath.Join(moduleRoot, path)
+	entries, err := os.ReadDir(filepath.Join(moduleRoot, path))
+	if err != nil {
+		slog.With("path", apiPath).Error("Failed to read files in api path", "error", err)
+		return
+	}
+
+	if clearTarget {
+		_ = os.RemoveAll(target)
+	}
+	err = os.MkdirAll(target, 0o755)
+	if err != nil {
+		slog.With("path", apiPath).Error("Failed to create api path dir", "error", err)
+		return
+	}
+
+	for _, e := range entries {
+		if keep(e.Name(), excludes) {
+			err = copyFile(filepath.Join(apiPath, e.Name()), filepath.Join(target, e.Name()))
+			if err != nil {
+				slog.With("path", apiPath, "file", e.Name()).Error("FFailed to copyFile from api path", "error", err)
+				return
+			}
+		}
+	}
+}
+
+func keep(name string, excludes []*regexp.Regexp) bool {
+	for _, exclude := range excludes {
+		if exclude.MatchString(name) {
+			return false
+		}
+	}
+	return true
+}
+
+func copyFile(src, dst string) error {
+	slog.With("from", src, "to", dst).Info("Copy file")
+	// Read all content of src to data, may cause OOM for a large file.
+	data, err := os.ReadFile(src)
+	if err != nil {
+		return err
+	}
+	// Write data to dst
+	return os.WriteFile(dst, data, 0o644)
+}
