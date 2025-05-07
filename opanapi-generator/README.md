085e563ef100a266dcea8f30532014e2d25e4bcc
bakito <github@bakito.ch>
2025-05-07 20:43:14 +0200

    reorganize files

---
+++ opanapi-generator/README.md
@@ -0,0 +1,7 @@
+# Generate struct from CRD
+
+run the following command in this directory
+
+```bash
+go generate
+```
--- opanapi-generator/generate.go
+++ opanapi-generator/generate.go
@@ -1,10 +1,13 @@
 //go:build generate
 // +build generate
 
+// run the generator
+//go:generate go run main.go mycrd.yaml v1alpha1/certificate.go
+
 // Generate deepcopy methodsets and CRD manifests
 //go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object paths=./v1alpha1
 
-package v1alpha1
+package main
 
 import (
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen" //nolint:typecheck
