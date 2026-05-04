beec84727e4f492d66242ca9657cb92debb19b13
bakito <github@bakito.ch>
2026-05-04 07:31:55 +0200

    fix

--- internal/render/render_test.go
+++ internal/render/render_test.go
@@ -2,11 +2,7 @@ package render
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
-var (
-	// verifies that all required dependencies for the application are correctly set up and available.
-	_ = scheme.Builder{}
-	_ = schema.GroupVersion{}
-)
+// verifies that all required dependencies for the application are correctly set up and available.
+var _ = schema.GroupVersion{}
